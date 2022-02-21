058c8f2165112f6d2bac4ee759132e78d59004bd
Nick Schwarzenberg <nick.schwarzenberg@tu-dresden.de>
2022-02-21 20:10:19 +0100

    emulator.py: Add support for bandwidth limiting, add parameter checks, cleaner packet handling

--- emulator.py
+++ emulator.py
@@ -11,6 +11,7 @@ import sys
 from socket import socket, AF_INET, SOCK_DGRAM
 from time import sleep, monotonic
 from random import random
+from math import inf
 
 start_time = monotonic()
 
@@ -20,6 +21,7 @@ start_time = monotonic()
 config = {
     'in': None,
     'out': None,
+    'speed': inf,
     'delay': 0.0,
     'per': 0.0,
 }
@@ -39,13 +41,38 @@ def printUsage():
     print("Usage:   "+sys.argv[0]+" in=value out=value [key=value ...]")
     print("         in     Local UDP port to receive packets from (mandatory)")
     print("         out    Local UDP port to send packets to (mandatory)")
-    print("         delay  Forwarding delay in seconds >=0 (default: 0.0)")
+    print("         speed  Bandwidth in bit per second > 0 (default: inf)")
+    print("         delay  Additional delay in seconds >= 0 (default: 0.0)")
     print("         per    Packet error rate 0...1 (default: 0.0)")
-    print("Example: "+sys.argv[0]+" in=1111 out=2222 delay=0.1")
+    print("Example: "+sys.argv[0]+" in=1111 out=2222 speed=56e3 delay=0.05")
 
 # ensure mandatory configuration options are set
 if config['in'] == None or config['out'] == None:
     printUsage()
+    exit(1)
+
+# enforce parameter types and check bounds
+try:
+    config['in'] = int(config['in'])
+    config['out'] = int(config['out'])
+except ValueError:
+    print("UDP ports must be integer values")
+    exit(1)
+try:
+    config['speed'] = float(config['speed'])
+    config['delay'] = float(config['delay'])
+    config['per'] = float(config['per'])
+except ValueError:
+    print("speed, delay and per must be parsable as float")
+    exit(1)
+if not config['speed'] > 0:
+    print("speed must be greater than zero")
+    exit(1)
+if not config['delay'] >= 0:
+    print("delay must be greater or equal than zero")
+    exit(1)
+if not (config['per'] >= 0 and config['per'] <= 1):
+    print("per must be between 0 and 1 (inclusive)")
     exit(1)
 
 def printWithTime(message):
@@ -56,26 +83,33 @@ def printWithTime(message):
 
 def handlePacket( data ):
 
-    printWithTime("Received %d bytes" % len(data))
+    printWithTime("Received packet of %d Bytes" % len(data))
 
-    delay = float(config['delay'])
-    printWithTime("Waiting %.3f seconds" % delay)
-    sleep(delay)
+    delay = config['delay']
+    if config['speed'] < inf:
+        # speed limit = delay depending on payload size
+        delay = delay + len(data)*8 / config['speed']
+    if delay > 0:
+        printWithTime("Waiting %.3f seconds" % delay)
+        sleep(delay)
 
-    if random() >= float(config['per']):
-        printWithTime("Forwarding packet")
-        return data
+    if config['per'] > 0:
+        if random() >= config['per']:
+            printWithTime("Forwarding packet")
+            return data
+        else:
+            printWithTime("Dropping packet")
+            return None
     else:
-        printWithTime("Dropping packet")
-        return None
+        return data
 
 
 ## Main Loop with UDP Payload Interface ##
 
 # create UDP listening socket
-udp = socket( AF_INET, SOCK_DGRAM )
-udp.bind(('127.0.0.1', int(config['in'])))
-printWithTime("Listening on port %d" % int(config['in']))
+udp = socket(AF_INET, SOCK_DGRAM)
+udp.bind(('127.0.0.1', config['in']))
+printWithTime("Listening on port %d" % config['in'])
 
 while True:
     data = None
@@ -89,4 +123,4 @@ while True:
     if incomingData:
         outgoingData = handlePacket( incomingData )
         if outgoingData:
-            udp.sendto(outgoingData, ('127.0.0.1', int(config['out'])))
+            udp.sendto(outgoingData, ('127.0.0.1', config['out']))
